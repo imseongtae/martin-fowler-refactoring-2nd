cd0395a7f5abe3587dbdf8034ac40293155560dd
imseongtae <dev.haemil@gmail.com>
2020-09-06 18:34:08 +0900

    test: Update printOwing.spec.js

--- chapter-06/part.01-Extract_Function/printOwing.spec.js
+++ chapter-06/part.01-Extract_Function/printOwing.spec.js
@@ -1,10 +1,10 @@
 // const { expect } = require('chai');
-// const { describe, it } = require('mocha');
 // const printOwing = require('./printOwing01');
 
 // const Clock = {
 // 	today: new Date(),
 // };
+
 console.log('test');
 
 describe('test', function () {
@@ -13,24 +13,3 @@ describe('test', function () {
 		done();
 	});
 });
-
-// describe('printOwing : 원래 함수', () => {
-// 	it('should print owing', () => {
-// 		let invoice = {
-// 			orders: [{ amount: 1 }],
-// 			customer: 'JL',
-// 		};
-
-// 		let expected =
-// 			'***********************\n' +
-// 			'**** Customer Owes ****\n' +
-// 			'***********************\n' +
-// 			'name: JL\n' +
-// 			'amount: 1\n' +
-// 			'due: 1/31/2000\n';
-
-// 		printOwing(invoice, console, Clock);
-
-// 		expect(console.content).to.equal(expected);
-// 	});
-// });
