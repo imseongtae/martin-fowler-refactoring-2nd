1a0dbb3f8d2270c8f9046b4de652bd6bbc9c07c8
imseongtae <dev.haemil@gmail.com>
2020-09-06 16:14:27 +0900

    test: Add set cost refactoring of chapter-01

--- chapter-01/set-cost-refactoring.js
+++ chapter-01/set-cost-refactoring.js
@@ -1,76 +1,101 @@
 const fs = require('fs');
+const plays = JSON.parse(fs.readFileSync('./plays.json').toString());
+const [invoices] = JSON.parse(fs.readFileSync('./invoices.json').toString());
+
+function state(invoice, plays) {
+	// return renderPlainText(createStatementData(invoice, plays));
+}
 
 // 공연료 청구서를 출력하는 코드
 function statement(invoice) {
-	let totalAmount = 0;
-	let volumeCredits = 0;
-	let result = `청구 내역 (고객명: ${invoice.customer})\n`;
-	const format = new Intl.NumberFormat('en-US', {
-		style: 'currency',
-		currency: 'USD',
-		minimumFractionDigits: 2,
-	}).format;
+	const statementData = {};
+	statementData.customer = invoice.customer;
+	statementData.performances = invoice.performances.map(enrichPerformance);
+	statementData.totalAmount = totalAmount(statementData);
+	statementData.totalVolumeCredits = totalVolumeCredits(statementData);
+	return renderPlainText(statementData, plays);
 
-	for (let perf of invoice.performances) {
-		// const play = playFor(perf); // 우변을 함수로 추출, 개별 공연에서 값을 얻음
-		// 추출한 함수를 사용
-		// let thisAmount = amountFor(perf);
-		// 포인트를 적립한다.
-		volumeCredits += Math.max(perf.audience - 30, 0);
-		// 희극 관객 5명마다 추가 포인트를 제공한다.
-		if ('comedy' === playFor(perf).type) {
-			volumeCredits += Math.floor(perf.audience / 5);
+	function enrichPerformance(aPerformance) {
+		const result = Object.assign({}, aPerformance); // 얕은 복사 수행
+		result.play = playFor(result);
+		result.amount = amountFor(result);
+		result.volumeCredits = volumeCreditsFor(result);
+
+		return result;
+	}
+	// totalAmount
+	function totalAmount(data) {
+		// for 반복문을 파이프라인으로 바꿈
+		return data.performances.reduce((total, p) => total + p.amount, 0);
+	}
+	function totalVolumeCredits(data) {
+		let result = 0; // 변수를 반복문 앞으로
+		for (let perf of data.performances) {
+			result += perf.volumeCredits;
 		}
-		// 청구 내역을 출력한다.
-		result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
-			perf.audience
-		}석)\n`;
-		totalAmount += amountFor(perf);
+		return result;
+	}
+
+	function volumeCreditsFor(aPerformance) {
+		let result = 0;
+		result += Math.max(aPerformance.audience - 30, 0);
+		// 희극 관객 5명마다 추가 포인트를 제공한다.
+		if ('comedy' === aPerformance.play.type) {
+			result += Math.floor(aPerformance.audience / 5);
+		}
+		return result;
+	}
+	function amountFor(aPerformance) {
+		// 함수명: (~에 대한 금액), 추출한 함수에는 그 코드가 하는 일을 설명하는 이름으로 지음
+		// 값이 바뀌지 않는 변수는 매개변수로 전달
+		let result = 0; // 명확한 이름으로 변경
+
+		switch (aPerformance.play.type) {
+			case 'tragedy': // 비극
+				result = 40000;
+				if (aPerformance.audience > 30) {
+					// result += 1000 * (aPerformance.audience - 30);
+					result += 1000 * (aPerformance.audience - 30);
+				}
+				break;
+			case 'comedy': // 희극
+				result = 30000;
+				if (aPerformance.audience > 20) {
+					result += 10000 + 500 * (aPerformance.audience - 20);
+				}
+				result += 300 * aPerformance.audience;
+				break;
+			default:
+				throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
+		}
+		return result; // 함수 안에서 값이 바뀌는 변수 반환
+	}
+	// 이를 통해 play 매개변수 제거
+	function playFor(aPerformance) {
+		return plays[aPerformance.playID];
+	}
+}
+
+// 청구 내역을 출력한다.
+function renderPlainText(data) {
+	let result = `청구 내역 (고객명: ${data.customer})\n`;
+	for (let perf of data.performances) {
+		result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
 	}
 
 	// return
-	result += `총액: ${format(totalAmount / 100)}\n`;
-	result += `적립 포인트: ${volumeCredits}점 \n`;
+	result += `총액: ${usd(data.totalAmount)}\n`;
+	result += `적립 포인트: ${data.totalVolumeCredits}점 \n`;
 	return result;
-}
 
-// const invoices = fs.readFileSync('./invoices.json');
-const invoices = JSON.parse(fs.readFileSync('./invoices.json').toString());
-const plays = JSON.parse(fs.readFileSync('./plays.json').toString());
-
-// console.log(invoices);
-const [invoicesData] = invoices;
-// statement(data, plays);
-console.log(statement(invoicesData, plays));
-
-// 함수명: (~에 대한 금액), 추출한 함수에는 그 코드가 하는 일을 설명하는 이름으로 지음
-// 값이 바뀌지 않는 변수는 매개변수로 전달
-function amountFor(aPerformance) {
-	let result = 0; // 명확한 이름으로 변경
-
-	switch (playFor(aPerformance).type) {
-		case 'tragedy': // 비극
-			result = 40000;
-			if (aPerformance.audience > 30) {
-				// result += 1000 * (aPerformance.audience - 30);
-				result += 1000 * (aPerformance.audience - 30);
-			}
-			break;
-		case 'comedy': // 희극
-			result = 30000;
-			if (aPerformance.audience > 20) {
-				result += 10000 + 500 * (aPerformance.audience - 20);
-			}
-			result += 300 * aPerformance.audience;
-			break;
-		default:
-			throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
+	// 화폐 단위 맞추기
+	function usd(aNumber) {
+		return new Intl.NumberFormat('en-US', {
+			style: 'currency',
+			currency: 'USD',
+			minimumFractionDigits: 2,
+		}).format(aNumber / 100);
 	}
-	return result; // 함수 안에서 값이 바뀌는 변수 반환
-}
+} // end renderPlainText
 
-// 임시변수를 질의함수로 바꾸기
-// 이를 통해 play 매개변수 제거
-function playFor(aPerformance) {
-	return plays[aPerformance.playID];
-}
+console.log(statement(invoices, plays));
