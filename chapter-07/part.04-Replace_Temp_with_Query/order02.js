6a1a75da55608a13ac2999a7ae2c7815a0f51f70
imseongtae <dev.haemil@gmail.com>
2020-09-08 22:44:32 +0900

    test: Add part.04

---
+++ chapter-07/part.04-Replace_Temp_with_Query/order02.js
@@ -0,0 +1,17 @@
+class Order {
+	constructor(quantity, item) {
+		this._quantity = quantity;
+		this._item = item;
+	}
+	get price() {
+		var discountFactor = 0.98;
+
+		if (this.basePrice > 1000) discountFactor -= 0.03;
+		return this.basePrice * discountFactor;
+	}
+	get basePrice() {
+		return this._quantity * this._item.price;
+	}
+}
+
+module.exports = Order;
