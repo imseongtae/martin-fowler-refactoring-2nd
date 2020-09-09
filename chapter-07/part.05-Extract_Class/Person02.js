df9cb59974dc2bda292feb1bafa086a25a8b27e7
imseongtae <dev.haemil@gmail.com>
2020-09-09 20:45:37 +0900

    test: Add part.05 of chapter-07

---
+++ chapter-07/part.05-Extract_Class/Person02.js
@@ -0,0 +1,43 @@
+class Person {
+	constructor() {
+		this._telephoneNumber = new TelephoneNumber();
+	}
+	get name() {
+		return this._name;
+	}
+	set name(arg) {
+		this._name = arg;
+	}
+	get telephoneNumber() {
+		return `(${this.officeAreaCode}) ${this.officeNumber}`;
+	}
+	get officeAreaCode() {
+		return this._telephoneNumber.officeAreaCode;
+	}
+	set officeAreaCode(arg) {
+		this._telephoneNumber.officeAreaCode = arg;
+	}
+	get officeNumber() {
+		return this._telephoneNumber.officeNumber;
+	}
+	set officeNumber(arg) {
+		this._telephoneNumber.officeNumber = arg;
+	}
+}
+
+class TelephoneNumber {
+	get officeAreaCode() {
+		return this._officeAreaCode;
+	}
+	set officeAreaCode(arg) {
+		this._officeAreaCode = arg;
+	}
+	get officeNumber() {
+		return this._officeNumber;
+	}
+	set officeNumber(arg) {
+		this._officeNumber = arg;
+	}
+}
+
+module.exports = Person;
---
+++ chapter-07/part.05-Extract_Class/Person03.js
@@ -0,0 +1,46 @@
+class Person {
+	constructor() {
+		this._telephoneNumber = new TelephoneNumber();
+	}
+	get name() {
+		return this._name;
+	}
+	set name(arg) {
+		this._name = arg;
+	}
+	get telephoneNumber() {
+		return this._telephoneNumber.telephoneNumber;
+	}
+	get officeAreaCode() {
+		return this._telephoneNumber.officeAreaCode;
+	}
+	set officeAreaCode(arg) {
+		this._telephoneNumber.officeAreaCode = arg;
+	}
+	get officeNumber() {
+		return this._telephoneNumber.officeNumber;
+	}
+	set officeNumber(arg) {
+		this._telephoneNumber.officeNumber = arg;
+	}
+}
+
+class TelephoneNumber {
+	get telephoneNumber() {
+		return `(${this.officeAreaCode}) ${this.officeNumber}`;
+	}
+	get officeAreaCode() {
+		return this._officeAreaCode;
+	}
+	set officeAreaCode(arg) {
+		this._officeAreaCode = arg;
+	}
+	get officeNumber() {
+		return this._officeNumber;
+	}
+	set officeNumber(arg) {
+		this._officeNumber = arg;
+	}
+}
+
+module.exports = Person;
