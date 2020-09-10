7173fe80a3421b0f294ba6b63ae61450178b2e61
imseongtae <dev.haemil@gmail.com>
2020-09-11 01:01:43 +0900

    test: Do refactoring that transfer TrackingInformation methods to Shipment class'

--- chapter-07/part.06-Inline_Class/after.js
+++ chapter-07/part.06-Inline_Class/after.js
@@ -4,26 +4,21 @@ class TrackingInformation {
 		this._trackingNumber = data.trackingNumber;
 	}
 	// 외부에서 직접 호출하는 TrackingInformation의 메서드들을 모조리 Shipment으로 옮기기
-	// get shippingCompany() {
-	// 	return this._shippingCompany;
+	// get trackingNumber() {
+	// 	return this._trackingNumber;
 	// }
-	// set shippingCompany(arg) {
-	// 	this._shippingCompany = arg;
+	// set trackingNumber(arg) {
+	// 	this._trackingNumber = arg;
 	// }
-	get trackingNumber() {
-		return this._trackingNumber;
-	}
-	set trackingNumber(arg) {
-		this._trackingNumber = arg;
-	}
-	get display() {
-		return `${this.shippingCompany}: ${this.trackingNumber}`;
-	}
+	// get display() {
+	// 	return `${this.shippingCompany}: ${this.trackingNumber}`;
+	// }
 }
 
 class Shipment {
 	constructor(data) {
 		this._shippingCompany = data.shippingCompany;
+		this._trackingNumber = data.trackingNumber;
 		this._trackingInformation = new TrackingInformation(data);
 	}
 	// Shipment에 위임 함수를 만들고, 클라이언트가 이를 호출하도록 수정
@@ -33,6 +28,12 @@ class Shipment {
 	set shippingCompany(arg) {
 		this._shippingCompany = arg;
 	}
+	get trackingNumber() {
+		return this._trackingNumber;
+	}
+	set trackingNumber(arg) {
+		this._trackingNumber = arg;
+	}
 	get trackingInfo() {
 		return this._trackingInformation.display;
 	}
@@ -41,6 +42,9 @@ class Shipment {
 	}
 	set trackingInformation(aTrackingInformation) {
 		this._trackingInformation = aTrackingInformation;
+	}
+	get display() {
+		return `${this.shippingCompany}: ${this.trackingNumber}`;
 	}
 }
 
