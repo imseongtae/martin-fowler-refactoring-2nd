a696c5c4c2fd597cc446ea190e36e7a491ec5e9c
imseongtae <dev.haemil@gmail.com>
2020-09-11 00:36:04 +0900

    test: Do Refactoring
    - 외부에서 직접 호출하는 TrackingInformation의 메서드들을 모조리 Shipment으로 옮기기
    - Shipment에 위임 함수를 만들고, 클라이언트가 이를 호출하도록 수정

--- chapter-07/part.06-Inline_Class/after.js
+++ chapter-07/part.06-Inline_Class/after.js
@@ -3,12 +3,13 @@ class TrackingInformation {
 		this._shippingCompany = data.shippingCompany;
 		this._trackingNumber = data.trackingNumber;
 	}
-	get shippingCompany() {
-		return this._shippingCompany;
-	}
-	set shippingCompany(arg) {
-		this._shippingCompany = arg;
-	}
+	// 외부에서 직접 호출하는 TrackingInformation의 메서드들을 모조리 Shipment으로 옮기기
+	// get shippingCompany() {
+	// 	return this._shippingCompany;
+	// }
+	// set shippingCompany(arg) {
+	// 	this._shippingCompany = arg;
+	// }
 	get trackingNumber() {
 		return this._trackingNumber;
 	}
@@ -22,7 +23,15 @@ class TrackingInformation {
 
 class Shipment {
 	constructor(data) {
+		this._shippingCompany = data.shippingCompany;
 		this._trackingInformation = new TrackingInformation(data);
+	}
+	// Shipment에 위임 함수를 만들고, 클라이언트가 이를 호출하도록 수정
+	get shippingCompany() {
+		return this._shippingCompany;
+	}
+	set shippingCompany(arg) {
+		this._shippingCompany = arg;
 	}
 	get trackingInfo() {
 		return this._trackingInformation.display;
@@ -35,6 +44,4 @@ class Shipment {
 	}
 }
 
-module.exports = {
-	Shipment,
-};
+module.exports = Shipment;
