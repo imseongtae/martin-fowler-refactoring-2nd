20288c5cafa954dca89af2390bcef400f0cc2429
imseongtae <dev.haemil@gmail.com>
2020-09-10 23:17:15 +0900

    feat: Add original code

---
+++ chapter-07/part.06-Inline_Class/after.js
@@ -0,0 +1,40 @@
+class TrackingInformation {
+	constructor(data) {
+		this._shippingCompany = data.shippingCompany;
+		this._trackingNumber = data.trackingNumber;
+	}
+	get shippingCompany() {
+		return this._shippingCompany;
+	}
+	set shippingCompany(arg) {
+		this._shippingCompany = arg;
+	}
+	get trackingNumber() {
+		return this._trackingNumber;
+	}
+	set trackingNumber(arg) {
+		this._trackingNumber = arg;
+	}
+	get display() {
+		return `${this.shippingCompany}: ${this.trackingNumber}`;
+	}
+}
+
+class Shipment {
+	constructor(data) {
+		this._trackingInformation = new TrackingInformation(data);
+	}
+	get trackingInfo() {
+		return this._trackingInformation.display;
+	}
+	get trackingInformation() {
+		return this._trackingInformation;
+	}
+	set trackingInformation(aTrackingInformation) {
+		this._trackingInformation = aTrackingInformation;
+	}
+}
+
+module.exports = {
+	Shipment,
+};
