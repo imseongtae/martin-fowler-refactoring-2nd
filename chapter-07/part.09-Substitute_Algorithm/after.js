ae4cf7c415ba946a1085e7b5e681d3b6634952c5
imseongtae <dev.haemil@gmail.com>
2020-09-15 23:13:36 +0900

    test: Do refactoring for substitute algorithm

--- chapter-07/part.09-Substitute_Algorithm/after.js
+++ chapter-07/part.09-Substitute_Algorithm/after.js
@@ -1,16 +1,6 @@
 function foundPerson(people) {
-	for (let i = 0; i < people.length; i++) {
-		if (people[i] === 'Don') {
-			return 'Don';
-		}
-		if (people[i] === 'John') {
-			return 'John';
-		}
-		if (people[i] === 'Kent') {
-			return 'Kent';
-		}
-	}
-	return '';
+	const candidates = ['Don', 'John', 'Kent'];
+	return people.find(p => candidates.includes(p) || '');
 }
 
 module.exports = foundPerson;
