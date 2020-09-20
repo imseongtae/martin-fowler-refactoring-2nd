84fd6b449cec59c6e25851a2297249cce476f009
imseongtae <dev.haemil@gmail.com>
2020-09-21 01:05:34 +0900

    test: Add Original Code

---
+++ chapter-08/part.02-Move_Field/before.js
@@ -0,0 +1,30 @@
+class Customer {
+	constructor(name, discountRate) {
+		this._name = name;
+		this._discountRate = discountRate;
+		this._contract = new CustomerContract(dateToday());
+	}
+	getdiscountRate() {
+		return this._discountRate;
+	}
+	becomePreferred() {
+		this._discountRate += 0.03;
+		//othernicethings
+	}
+
+	applyDiscount(amount) {
+		return amount.subtract(amount.multiply(this._discountRate));
+	}
+}
+
+function dateToday() {
+	return new Date();
+}
+
+class CustomerContract {
+	constructor(startDate) {
+		this._startDate = startDate;
+	}
+}
+
+module.exports = Customer;
