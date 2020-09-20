62b35c250d319a7e5fd4a6b4cbf7e4666a4fa4ec
imseongtae <dev.haemil@gmail.com>
2020-09-21 01:26:17 +0900

    test: Customer 메서드 추가, CustomerContract 클래스에 필드와 접근자 추가

--- chapter-08/part.02-Move_Field/after.js
+++ chapter-08/part.02-Move_Field/after.js
@@ -1,14 +1,19 @@
 class Custoemer {
 	constructor(name, discountRate) {
 		this._name = name;
-		this._discountRate = discountRate;
+		// this._discountRate = discountRate;
+		this._setDiscountRate(discountRate);
 		this._contract = new CustomerContract(dateToday());
 	}
 	get discountRate() {
 		return this._discountRate;
 	}
+	// 할인율을 수정하는 public 세터 대신 메서드를 이용
+	_setDiscountRate(aNumber) {
+		this._discountRate = aNumber;
+	}
 	becomePreferred() {
-		this._discountRate += 0.03;
+		this._setDiscountRate(this.discountRate + 0.03);
 	}
 
 	applyDiscount(amount) {
@@ -16,9 +21,17 @@ class Custoemer {
 	}
 }
 
+// CustomerContract 클래스에 필드와 접근자들을 추가
 class CustomerContract {
-	constructor(startDate) {
+	constructor(startDate, discountRate) {
 		this._startDate = startDate;
+		this._discountRate = discountRate;
+	}
+	get discountRate() {
+		return this._discountRate;
+	}
+	set discountRate(arg) {
+		this._discountRate = arg;
 	}
 }
 
