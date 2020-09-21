ebe0ae76e98eae63782f6c02ab1c474fb2e7a11b
imseongtae <dev.haemil@gmail.com>
2020-09-22 05:35:28 +0900

    test: Update customer.spec to include refactoring code test

--- chapter-08/part.02-Move_Field/test/customer.spec.js
+++ chapter-08/part.02-Move_Field/test/customer.spec.js
@@ -1,10 +1,20 @@
 const { expect } = require('chai');
 const beforeCustomer = require('../before');
+const afterCustomer = require('../after');
 const Money = require('js-money');
 
-describe('Customer', () => {
+describe('8-2 Original Customer', () => {
 	it('calculates discount', () => {
 		const customer = new beforeCustomer('Mike', 0.05);
+		expect(customer.applyDiscount(new Money(500, Money.EUR))).to.eql(
+			new Money(475, Money.EUR),
+		);
+	});
+});
+
+describe('8-2 Refactoring Customer', () => {
+	it('calculates discount', () => {
+		const customer = new afterCustomer('Mike', 0.05);
 		expect(customer.applyDiscount(new Money(500, Money.EUR))).to.eql(
 			new Money(475, Money.EUR),
 		);
