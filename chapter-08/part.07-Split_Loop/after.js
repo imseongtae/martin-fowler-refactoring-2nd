a4230517a0da493eb85272ff61485d0956ded9b1
imseongtae <dev.haemil@gmail.com>
2020-09-23 06:52:32 +0900

    test: 반복문 쪼개기, 단순 반복문 복제 이후 중복 제거

--- chapter-08/part.07-Split_Loop/after.js
+++ chapter-08/part.07-Split_Loop/after.js
@@ -2,8 +2,15 @@ function reportYoungestAgeAndTotalSalary(people) {
 	let youngest = people[0] ? people[0].age : Infinity;
 	let totalSalary = 0;
 	for (const p of people) {
-		if (p.age < youngest) youngest = p.age;
+		// if (p.age < youngest) youngest = p.age;
 		totalSalary += p.salary;
+	}
+	// 반복문 쪼개기 단계
+	// 1. 단순히 반복문 복제하기
+	// 2. 잘못된 결과를 초래할 수 있는 중복 제거
+	for (const p of people) {
+		if (p.age < youngest) youngest = p.age;
+		// totalSalary += p.salary;
 	}
 	return `youngestAge: ${youngest}, totalSalary: ${totalSalary}`;
 }
