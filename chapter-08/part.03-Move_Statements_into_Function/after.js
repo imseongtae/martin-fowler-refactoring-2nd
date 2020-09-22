f3638a8f82186578203d1af028da2f76d82145c1
imseongtae <dev.haemil@gmail.com>
2020-09-22 23:41:26 +0900

    test: emitPhotoData함수를 인라인 후 임시 함수 이름을 emitPhotoData로 변경

--- chapter-08/part.03-Move_Statements_into_Function/after.js
+++ chapter-08/part.03-Move_Statements_into_Function/after.js
@@ -2,27 +2,30 @@ function renderPerson(outStream, person) {
 	const result = [];
 	result.push(`<p>${person.name}</p>`);
 	// result.push(renderPhoto(person.photo));
-	// result.push(`<p>title: ${person.photo.title}</p>`);
-	// result.push(emitPhotoData(person.photo));
-	result.push(zznew(person.photo));
+	result.push(emitPhotoData(person.photo)); // 1. 호출자 수정 후
 	return result.join('\n');
 }
 
 function photoDiv(p) {
-	return ['<div>', zznew(p), '</div>'].join('\n');
+	return ['<div>', emitPhotoData(p), '</div>'].join('\n');
 }
 
-// emitPhotoData로 옮기려는 코드와 호출문을 함께 추출
-function zznew(p) {
-	return [`<p>title: ${p.title}</p>`, emitPhotoData(p)].join('\n');
+// emitPhotoData 함수를 인라인 후
+// zznew 이름을 emitPhotoData로 변경
+function emitPhotoData(p) {
+	return [
+		`<p>title: ${p.title}</p>`,
+		`<p>location: ${p.location}</p>`,
+		`<p>date: ${p.date.toDateString()}</p>`,
+	].join('\n');
 }
 
-function emitPhotoData(aPhoto) {
-	const result = [];
-	result.push(`<p>location: ${aPhoto.location}</p>`);
-	result.push(`<p>date: ${aPhoto.date.toDateString()}</p>`);
-	return result.join('\n');
-}
+// function emitPhotoData(aPhoto) {
+// 	const result = [];
+// 	result.push(`<p>location: ${aPhoto.location}</p>`);
+// 	result.push(`<p>date: ${aPhoto.date.toDateString()}</p>`);
+// 	return result.join('\n');
+// }
 
 const date = new Date('May 10, 2019');
 
