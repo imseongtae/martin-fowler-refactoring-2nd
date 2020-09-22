2bbd395340a93cc8c334c119b8eff284f5760f0c
imseongtae <dev.haemil@gmail.com>
2020-09-22 23:27:34 +0900

    test: Add code before refactoring

---
+++ chapter-08/part.03-Move_Statements_into_Function/after.js
@@ -0,0 +1,37 @@
+function renderPerson(outStream, person) {
+	const result = [];
+	result.push(`<p>${person.name}</p>`);
+	// result.push(renderPhoto(person.photo));
+	result.push(`<p>title: ${person.photo.title}</p>`);
+	result.push(emitPhotoData(person.photo));
+	return result.join('\n');
+}
+
+function photoDiv(p) {
+	return ['<div>', `<p>title: ${p.title}</p>`, emitPhotoData(p), '</div>'].join('\n');
+}
+
+function emitPhotoData(aPhoto) {
+	const result = [];
+	result.push(`<p>location: ${aPhoto.location}</p>`);
+	result.push(`<p>date: ${aPhoto.date.toDateString()}</p>`);
+	return result.join('\n');
+}
+
+const date = new Date('May 10, 2019');
+
+const aPerson = {
+	name: 'Alex',
+	photo: {
+		title: 'holiday',
+		location: 'Greece',
+		date: date,
+	},
+};
+
+console.log(renderPerson([], aPerson));
+
+module.exports = {
+	renderPerson,
+	photoDiv,
+};
