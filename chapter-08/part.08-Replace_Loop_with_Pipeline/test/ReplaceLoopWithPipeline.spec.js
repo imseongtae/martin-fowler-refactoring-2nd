cb68f09903b698364e24aa685c889bdc9a16fd17
imseongtae <dev.haemil@gmail.com>
2020-10-07 20:46:18 +0900

    test: Add part.08

---
+++ chapter-08/part.08-Replace_Loop_with_Pipeline/test/ReplaceLoopWithPipeline.spec.js
@@ -0,0 +1,24 @@
+const { expect } = require('chai');
+const beforeAcquireData = require('../before');
+
+const csvInput = `office, country, telephone\n
+Chicago, USA, +1 312 373 1000\n
+Beijing, China, +86 4008 900 505\n
+Bangalore, India, +91 80 4064 9570\n
+Porto Alegre, Brazil, +55 51 3079 3550\n
+Chennai, India, +91 44 660 44766`;
+
+describe('8-8 Original ReplaceLoopWithPipeline', () => {
+	it('pick India cities only', () => {
+		expect(beforeAcquireData(csvInput)).to.eql([
+			{
+				city: 'Bangalore',
+				phone: '+91 80 4064 9570',
+			},
+			{
+				city: 'Chennai',
+				phone: '+91 44 660 44766',
+			},
+		]);
+	});
+});
