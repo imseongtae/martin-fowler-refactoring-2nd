93e30c37afd913605beadd6421aafb955280e29c
imseongtae <dev.haemil@gmail.com>
2020-10-08 14:51:40 +0900

    test: 변수 result에 결과를 할당, 파이프라인의 결과를 누적 변수에 대입하여 코드를 줄임

--- chapter-08/part.08-Replace_Loop_with_Pipeline/after.js
+++ chapter-08/part.08-Replace_Loop_with_Pipeline/after.js
@@ -10,18 +10,19 @@ Chennai, India, +91 44 660 44766
 function acquireData(input) {
 	const lines = input.split('\n');
 	// let firstLine = true; => slice 연산을 떠올리게 함
-	const result = [];
-	const loopItems = lines
+	// const result = []; 변수 result에 결과를 할당
+	const result = lines
 		.slice(1)
 		.filter(line => line.trim() !== '')
 		.map(line => line.split(',')) // map 연산을 사용해 여러 줄짜리 데이터를 문자열 배열로 반환
 		.filter(record => record[1].trim() === 'India') // 인도에 위치한 사무실 레코드를 뽑아낸다.
 		.map(record => ({ city: record[0].trim(), phone: record[2].trim() }));
-	//
-	for (const line of loopItems) {
-		const record = line;
-		result.push(line);
-	}
+
+	// 파이프라인의 결과를 누적 변수에 대입하면 아래 코드도 제거할 수 있음
+	// for (const line of loopItems) {
+	// 	const record = line;
+	// 	result.push(line);
+	// }
 	return result;
 }
 
