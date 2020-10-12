3e6636a5b67f3e461144e92beb4e697be8efbb39
imseongtae <dev.haemil@gmail.com>
2020-10-12 23:34:16 +0900

    refactor: 별도의 필드를 정의하고, 생성자와 접근자에서 둘을 구분해 사용하기

--- chapter-09/part.02-Rename_Field/after.js
+++ chapter-09/part.02-Rename_Field/after.js
@@ -1,13 +1,14 @@
+// 별도의 필드를 정의하고, 생성자와 접근자에서 둘을 구분해 사용하기
 class Organization {
 	constructor(data) {
-		this._name = data.name;
+		this._title = data.name;
 		this._country = data.country;
 	}
 	get name() {
-		return this._name;
+		return this._title;
 	}
 	set name(aString) {
-		this._name = aString;
+		this._title = aString;
 	}
 	get country() {
 		return this._country;
