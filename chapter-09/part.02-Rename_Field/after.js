0b9675b272e55d2198e2eb2ea2c1014e9c79a4c1
imseongtae <dev.haemil@gmail.com>
2020-10-12 23:37:56 +0900

    refactor: 생성자에서 title도 받아들일 수 있도록 조치, 이를 통해 title과 name 둘 다 사용할 수 있고, title이 우선함

--- chapter-09/part.02-Rename_Field/after.js
+++ chapter-09/part.02-Rename_Field/after.js
@@ -1,7 +1,8 @@
 // 별도의 필드를 정의하고, 생성자와 접근자에서 둘을 구분해 사용하기
 class Organization {
 	constructor(data) {
-		this._title = data.name;
+		// 생성자에서 'title'도 받아들일 수 있도록 조치, 이를 통해 title과 name 둘 다 사용할 수 있고, title이 우선함
+		this._title = data.title !== undefined ? data.title : data.name;
 		this._country = data.country;
 	}
 	get name() {
