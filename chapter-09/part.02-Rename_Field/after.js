fb0a71389d0022dc14564b4badbb9d06adbf72ab
imseongtae <dev.haemil@gmail.com>
2020-10-12 23:30:38 +0900

    refactor: organization 레코드를 클래스로 캡슐화

--- chapter-09/part.02-Rename_Field/after.js
+++ chapter-09/part.02-Rename_Field/after.js
@@ -1,1 +1,23 @@
-const organization = { name: 'Acme Gooseberries', country: 'GB' };
+class Organization {
+	constructor(data) {
+		this._name = data.name;
+		this._country = data.country;
+	}
+	get name() {
+		return this._name;
+	}
+	set name(aString) {
+		this._name = aString;
+	}
+	get country() {
+		return this._country;
+	}
+	set country(aCountryCode) {
+		this._country = aCountryCode;
+	}
+}
+
+// organization 레코드를 클래스로 캡슐화
+const organization = new Organization({ name: 'Acme Gooseberries', country: 'GB' });
+
+module.exports = organization;
