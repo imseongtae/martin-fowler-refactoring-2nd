958d914d847099fd40803cabfc9f3073fd5af453
imseongtae <dev.haemil@gmail.com>
2020-10-14 01:14:38 +0900

    변수 쪼개기 적용 이후, 어서션을 추가

--- chapter-09/part.03-Replace_Derived_Variable_with_Query/ProductionPlan.js
+++ chapter-09/part.03-Replace_Derived_Variable_with_Query/ProductionPlan.js
@@ -1,3 +1,5 @@
+const { assert } = require('chai');
+
 class ProductionPlan {
 	constructor(production) {
 		// 변수 쪼개기를 적용
@@ -6,8 +8,12 @@ class ProductionPlan {
 		this._adjustments = [];
 	}
 	get production() {
-		// 변수 쪼개기를 적용
+		// 변수 쪼개기 적용 이후, 어서션을 추가
+		assert(this._productionAccumulator === this.calculatedProductionAccumulator);
 		return this._initialProduction + this._productionAccumulator;
+	}
+	get calculatedProductionAccumulator() {
+		return this._adjustments.reduce((sum, a) => sum + a.amount, 0);
 	}
 	applyAdjustment(anAdjustment) {
 		this._adjustments.push(anAdjustment);
