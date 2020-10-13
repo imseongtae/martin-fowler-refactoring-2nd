838299c08584d4d321722cc3bd47a551303bb461
imseongtae <dev.haemil@gmail.com>
2020-10-14 00:10:28 +0900

    refactor: assert를 추가하여 검증하기

--- chapter-09/part.03-Replace_Derived_Variable_with_Query/after.js
+++ chapter-09/part.03-Replace_Derived_Variable_with_Query/after.js
@@ -1,10 +1,17 @@
+const { assert } = require('chai');
+
 class ProductionPlan {
 	constructor(data) {
 		this._adjustments = data.adjustments;
 		this._production = data.production;
 	}
 	get production() {
+		// assert를 추가하여 검증하기
+		assert(this._production === this.calculateProduction);
 		return this._production;
+	}
+	get calculateProduction() {
+		return this._adjustments.reduce((sum, a) => sum + a.amount, 0);
 	}
 	applyAdjustment(anAdjustment) {
 		this._adjustments.push(anAdjustment);
