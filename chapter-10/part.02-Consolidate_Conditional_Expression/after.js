91339dc348ed1583da5407d025f27fe674956390
imseongtae <dev.haemil@gmail.com>
2020-10-19 03:10:01 +0900

    test: 모든 조건식 통합

--- chapter-10/part.02-Consolidate_Conditional_Expression/after.js
+++ chapter-10/part.02-Consolidate_Conditional_Expression/after.js
@@ -1,7 +1,8 @@
 function disabilityAmount(anEmployee) {
 	// 조건 검사가 순차적인 경우 or 연산자를 이용하여 조건식 통합
-	if (anEmployee.seniority < 2 || anEmployee.monthsDisabled > 12) return 0;
-	if (anEmployee.isPartTime) return 0;
+	// 모든 조건식 통합
+	if (anEmployee.seniority < 2 || anEmployee.monthsDisabled > 12 || anEmployee.isPartTime)
+		return 0;
 	// compute the disability amount, 장애 수당 계산
 }
 
