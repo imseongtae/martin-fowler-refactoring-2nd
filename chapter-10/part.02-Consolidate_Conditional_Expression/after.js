4e42834ccb1475be88677f71c069ac6fcc1fb9f0
imseongtae <dev.haemil@gmail.com>
2020-10-19 04:26:11 +0900

    test: 모든 조건을 통합했다면 최종 조건식을 함수로 추출

--- chapter-10/part.02-Consolidate_Conditional_Expression/after.js
+++ chapter-10/part.02-Consolidate_Conditional_Expression/after.js
@@ -1,9 +1,14 @@
 function disabilityAmount(anEmployee) {
-	// 조건 검사가 순차적인 경우 or 연산자를 이용하여 조건식 통합
-	// 모든 조건식 통합
-	if (anEmployee.seniority < 2 || anEmployee.monthsDisabled > 12 || anEmployee.isPartTime)
-		return 0;
+	if (isNotEligibleForDisability()) return 0;
 	// compute the disability amount, 장애 수당 계산
+
+	// 모든 조건을 통합했다면 최종 조건식을 함수로 추출
+	function isNotEligibleForDisability() {
+		// 조건 검사가 순차적인 경우 or 연산자를 이용하여 조건식 통합
+		return (
+			anEmployee.seniority < 2 || anEmployee.monthsDisabled > 12 || anEmployee.isPartTime
+		);
+	}
 }
 
 module.exports = disabilityAmount;
