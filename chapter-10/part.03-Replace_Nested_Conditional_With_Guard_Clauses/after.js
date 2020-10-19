3d672f65822214a5e13d6d8ff68f8ea43079c21d
imseongtae <dev.haemil@gmail.com>
2020-10-20 02:12:14 +0900

    test: 아무 일도 하지 않는 result 변수 제거

--- chapter-10/part.03-Replace_Nested_Conditional_With_Guard_Clauses/after.js
+++ chapter-10/part.03-Replace_Nested_Conditional_With_Guard_Clauses/after.js
@@ -1,6 +1,6 @@
 // 코드가 의도한 일은 모든 조건이 거짓일 때만 실행되므로, 보호 구문 사용을 통해 의도를 드러내기
 function payAmount(employee) {
-	let result;
+	// let result; // 아무 일도 하지 않는 result 변수 제거
 	// 퇴사한 직원인가?
 	if (employee.isSeparated) return { amount: 0, reasonCode: 'SEP' };
 	// 은퇴한 직원인가?
@@ -10,8 +10,7 @@ function payAmount(employee) {
 	// consectetur(adipiscing).elit();
 	// sed.do.eiusmod = tempor.incididunt.ut(labore) && dolore(magna.aliqua);
 	// ut.enim.ad(minim.veniam);
-	result = someFinalComputation();
-	return result;
+	return someFinalComputation();
 }
 
 function someFinalComputation() {
