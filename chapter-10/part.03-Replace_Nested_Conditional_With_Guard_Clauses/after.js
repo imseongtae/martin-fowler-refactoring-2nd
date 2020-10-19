e3c1b353b3038b4b8d7dca6e7b7c6ebabd7da37c
imseongtae <dev.haemil@gmail.com>
2020-10-20 02:00:16 +0900

    test: 코드가 의도한 일은 모든 조건이 거짓일 때만 실행되므로, 보호 구문 사용을 통해 의도를 드러내기

--- chapter-10/part.03-Replace_Nested_Conditional_With_Guard_Clauses/after.js
+++ chapter-10/part.03-Replace_Nested_Conditional_With_Guard_Clauses/after.js
@@ -1,20 +1,18 @@
+// 코드가 의도한 일은 모든 조건이 거짓일 때만 실행되므로, 보호 구문 사용을 통해 의도를 드러내기
 function payAmount(employee) {
 	let result;
-	if (employee.isSeparated) {
-		// 퇴사한 직원인가?
-		result = { amount: 0, reasonCode: 'SEP' };
+	// 퇴사한 직원인가?
+	if (employee.isSeparated) result = { amount: 0, reasonCode: 'SEP' };
+	// 은퇴한 직원인가?
+	if (employee.isRetired) {
+		result = { amount: 0, reasonCode: 'RET' };
 	} else {
-		if (employee.isRetired) {
-			// 은퇴한 직원인가?
-			result = { amount: 0, reasonCode: 'RET' };
-		} else {
-			// logic to compute amount, 급여 계산 로직
-			// lorem.ipsum(dolor.sitAmet);
-			// consectetur(adipiscing).elit();
-			// sed.do.eiusmod = tempor.incididunt.ut(labore) && dolore(magna.aliqua);
-			// ut.enim.ad(minim.veniam);
-			result = someFinalComputation();
-		}
+		// logic to compute amount, 급여 계산 로직
+		// lorem.ipsum(dolor.sitAmet);
+		// consectetur(adipiscing).elit();
+		// sed.do.eiusmod = tempor.incididunt.ut(labore) && dolore(magna.aliqua);
+		// ut.enim.ad(minim.veniam);
+		result = someFinalComputation();
 	}
 	return result;
 }
