64447cb8d0623b59b6f54e7cf265ffe1df43a60e
imseongtae <dev.haemil@gmail.com>
2020-10-20 02:46:55 +0900

    test: not 연산자를 통해 분리한 구문을 간소화(조건을 역으로)

--- chapter-10/part.03-Replace_Nested_Conditional_With_Guard_Clauses/AdjustedCapital/after.js
+++ chapter-10/part.03-Replace_Nested_Conditional_With_Guard_Clauses/AdjustedCapital/after.js
@@ -3,8 +3,8 @@ function adjustedCapital(anInstrument) {
 	let result = 0;
 	// 보호 구문을 추가하면서 조건을 역으로 바꿈
 	if (anInstrument.capital <= 0) return result;
-	// 먼저 간단히 not 연산자를 추가하고 구문을 분리
-	if (!(anInstrument.interestRate > 0 && anInstrument.duration > 0)) return result;
+	// not 연산자를 통해 분리한 구문을 간소화(조건을 역으로)
+	if (anInstrument.interestRate <= 0 || anInstrument.duration <= 0) return result;
 	result = (anInstrument.income / anInstrument.duration) * anInstrument.adjustmentFactor;
 	return result;
 }
