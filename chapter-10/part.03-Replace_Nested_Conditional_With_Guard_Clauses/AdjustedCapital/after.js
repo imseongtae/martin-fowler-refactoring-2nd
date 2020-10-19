6e9213fc6ac929203b1844e7627403d6bf4968e7
imseongtae <dev.haemil@gmail.com>
2020-10-20 02:36:24 +0900

    test: 보호 구문을 추가하면서 조건을 역으로 바꿈

--- chapter-10/part.03-Replace_Nested_Conditional_With_Guard_Clauses/AdjustedCapital/after.js
+++ chapter-10/part.03-Replace_Nested_Conditional_With_Guard_Clauses/AdjustedCapital/after.js
@@ -1,10 +1,11 @@
+// 보호구문으로 바꾸는 리팩터링을 수행할 때 조건을 역으로 만드는 경우
 function adjustedCapital(anInstrument) {
 	let result = 0;
-	if (anInstrument.capital > 0) {
-		if (anInstrument.interestRate > 0 && anInstrument.duration > 0) {
-			result =
-				(anInstrument.income / anInstrument.duration) * anInstrument.adjustmentFactor;
-		}
+	// 보호 구문을 추가하면서 조건을 역으로 바꿈
+	if (anInstrument.capital <= 0) return result;
+	if (anInstrument.interestRate > 0 && anInstrument.duration > 0) {
+		result =
+			(anInstrument.income / anInstrument.duration) * anInstrument.adjustmentFactor;
 	}
 	return result;
 }
