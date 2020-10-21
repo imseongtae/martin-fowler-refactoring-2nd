e910298adf02aaa8f0103ea6de3974bf3e132401
imseongtae <dev.haemil@gmail.com>
2020-10-21 16:19:23 +0900

    refactor: plumage, airSpeedVelocity 함수를 클래스로 묶기

--- chapter-10/part.04-Replace_Conditional_With_Polymorphism/Bird/after.js
+++ chapter-10/part.04-Replace_Conditional_With_Polymorphism/Bird/after.js
@@ -1,33 +1,40 @@
-function plumages(birds) {
-	return new Map(birds.map(b => [b.name, plumage(b)]));
+function plumages(bird) {
+	// return new Map(birds.map(b => [b.name, plumage(b)]));
+	return new Bird(bird).plumage;
 }
-function speeds(birds) {
-	return new Map(birds.map(b => [b.name, airSpeedVelocity(b)]));
+function speeds(bird) {
+	// return new Map(birds.map(b => [b.name, airSpeedVelocity(b)]));
+	return new Bird(bird).airSpeedVelocity;
 }
 
-function plumage(bird) {
-	switch (bird.type) {
-		case 'EuropeanSwallow':
-			return 'average';
-		case 'AfricanSwallow':
-			return bird.numberOfCoconuts > 2 ? 'tired' : 'average';
-		case 'NorwegianBlueParrot':
-			return bird.voltage > 100 ? 'scorched' : 'beautiful';
-		default:
-			return 'unknown';
+// plumage, airSpeedVelocity 함수를 클래스로 묶기
+class Bird {
+	constructor(birdObject) {
+		Object.assign(this, birdObject);
 	}
-}
-
-function airSpeedVelocity(bird) {
-	switch (bird.type) {
-		case 'EuropeanSwallow':
-			return 35;
-		case 'AfricanSwallow':
-			return 40 - 2 * bird.numberOfCoconuts;
-		case 'NorwegianBlueParrot':
-			return bird.isNailed ? 0 : 10 + bird.voltage / 10;
-		default:
-			return null;
+	get plumage() {
+		switch (this.type) {
+			case 'EuropeanSwallow':
+				return 'average';
+			case 'AfricanSwallow':
+				return this.numberOfCoconuts > 2 ? 'tired' : 'average';
+			case 'NorwegianBlueParrot':
+				return this.voltage > 100 ? 'scorched' : 'beautiful';
+			default:
+				return 'unknown';
+		}
+	}
+	get airSpeedVelocity() {
+		switch (this.type) {
+			case 'EuropeanSwallow':
+				return 35;
+			case 'AfricanSwallow':
+				return 40 - 2 * this.numberOfCoconuts;
+			case 'NorwegianBlueParrot':
+				return this.isNailed ? 0 : 10 + this.voltage / 10;
+			default:
+				return null;
+		}
 	}
 }
 
