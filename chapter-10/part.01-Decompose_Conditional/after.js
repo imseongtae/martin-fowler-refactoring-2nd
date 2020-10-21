130d2c71ad57746c4b453ea97b68e9c486c1bb70
imseongtae <dev.haemil@gmail.com>
2020-10-21 21:46:16 +0900

    refactor: 여름철을 계산하는 조건식을 별도 함수로 추출

--- chapter-10/part.01-Decompose_Conditional/after.js
+++ chapter-10/part.01-Decompose_Conditional/after.js
@@ -1,11 +1,16 @@
+// 여름철이면 할인율이 달라지는 서비스의 요금을 계산하는 코드
 function getCharge(aDate, plan, quantity) {
 	let charge;
-	// 조건 부분
-	if (!aDate.isBefore(plan.summerStart) && !aDate.isAfter(plan.summerEnd))
-		charge = quantity * plan.summerRate;
+	// 여름철을 계산하는 함수
+	if (summer()) charge = quantity * plan.summerRate;
 	else charge = quantity * plan.regularRate + plan.regularServiceCharge;
 
 	return charge;
+
+	// 여름철을 계산하는 조건식을 별도 함수로 추출
+	function summer() {
+		return !aDate.isBefore(plan.summerStart) && !aDate.isAfter(plan.summerEnd);
+	}
 }
 
 module.exports = getCharge;
