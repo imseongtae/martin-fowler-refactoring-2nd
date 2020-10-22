cbd09b35dbc1146afb8037aa1da6fc09bf4f534a
imseongtae <dev.haemil@gmail.com>
2020-10-23 02:45:23 +0900

    refactor: 다형성을 적용하기 위해 세부 계산 함수들을 Rating 클래스로 묶기

--- chapter-10/part.04-Replace_Conditional_With_Polymorphism/Rating/after.js
+++ chapter-10/part.04-Replace_Conditional_With_Polymorphism/Rating/after.js
@@ -1,51 +1,59 @@
 // 투자 등급
 // rating 함수는 위험 요인과 수익 요인을 종합하여 요청한 항해의 최종등급을 계산
 function rating(voyage, history) {
-	const vpf = voyageProfitFactor(voyage, history);
-	const vr = voyageRisk(voyage);
-	const chr = captainHistoryRisk(voyage, history);
-	if (vpf * 3 > vr + chr * 2) return 'A';
-	else return 'B';
+	return new Rating(voyage, history).value;
 }
 
-// 항해 경로 위험요소
-function voyageRisk(voyage) {
-	let result = 1;
-	if (voyage.length > 4) result += 2;
-	if (voyage.length > 8) result += voyage.length - 8;
-	if (['china', 'east­indies'].includes(voyage.zone)) result += 4;
-	return Math.max(result, 0);
-}
-
-// 선장의 항해 이력 위험요소
-function captainHistoryRisk(voyage, history) {
-	let result = 1;
-	if (history.length < 5) result += 4;
-	result += history.filter(v => v.profit < 0).length;
-	if (voyage.zone === 'china' && hasChina(history)) result -= 2;
-	return Math.max(result, 0);
-}
-
-// 중국을 경유하는가?
-function hasChina(history) {
-	return history.some(v => 'china' === v.zone);
-}
-
-// 수익 요인
-function voyageProfitFactor(voyage, history) {
-	let result = 2;
-	if (voyage.zone === 'china') result += 1;
-	if (voyage.zone === 'east­indies') result += 1;
-	if (voyage.zone === 'china' && hasChina(history)) {
-		result += 3;
-		if (history.length > 10) result += 1;
-		if (voyage.length > 12) result += 1;
-		if (voyage.length > 18) result -= 1;
-	} else {
-		if (history.length > 8) result += 1;
-		if (voyage.length > 14) result -= 1;
+// 다형성을 적용하기 위해 세부 계산 함수들을 Rating 클래스로 묶기
+class Rating {
+	constructor(voyage, history) {
+		this.voyage = voyage;
+		this.history = history;
 	}
-	return result;
+	get value() {
+		const vpf = this.voyageProfitFactor;
+		const vr = this.voyageRisk;
+		const chr = this.captainHistoryRisk;
+		if (vpf * 3 > vr + chr * 2) return 'A';
+		else return 'B';
+	}
+	// 항해 경로 위험요소
+	get voyageRisk() {
+		let result = 1;
+		if (this.voyage.length > 4) result += 2;
+		if (this.voyage.length > 8) result += this.voyage.length - 8;
+		if (['china', 'east­indies'].includes(this.voyage.zone)) result += 4;
+		return Math.max(result, 0);
+	}
+	// 선장의 항해 이력 위험요소
+	get captainHistoryRisk() {
+		let result = 1;
+		if (this.history.length < 5) result += 4;
+		result += this.history.filter(v => v.profit < 0).length;
+		if (this.voyage.zone === 'china' && this.hasChinaHistory) result -= 2;
+		return Math.max(result, 0);
+	}
+	// 수익 요인
+	get voyageProfitFactor() {
+		let result = 2;
+		if (this.voyage.zone === 'china') result += 1;
+		if (this.voyage.zone === 'east­indies') result += 1;
+		if (this.voyage.zone === 'china' && this.hasChinaHistory) {
+			result += 3;
+			if (this.history.length > 10) result += 1;
+			if (this.voyage.length > 12) result += 1;
+			if (this.voyage.length > 18) result -= 1;
+		} else {
+			if (this.history.length > 8) result += 1;
+			if (this.voyage.length > 14) result -= 1;
+		}
+		return result;
+	}
+	// 중국을 경유하는가?
+	get hasChinaHistory() {
+		// some() 메서드를 통해 배열 안 요소가 주어진 판별 함수를 통과하는지 테스트
+		return this.history.some(v => 'china' === v.zone);
+	}
 }
 
 module.exports = rating;
