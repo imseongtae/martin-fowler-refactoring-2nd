4ebc05697daa3da28289a700fb4e1f9a5696a41d
imseongtae <dev.haemil@gmail.com>
2020-10-24 02:16:51 +0900

    refactor: 어서션(양수라는 가정을 명시)을 넣기 위해 if-then 문장으로 재구성

--- chapter-10/part.06-Introduce_Assertion/after.js
+++ chapter-10/part.06-Introduce_Assertion/after.js
@@ -2,8 +2,10 @@ class Customer {
 	constructor(data) {
 		this.discountRate = data.discountRate;
 	}
+	// 어서션(양수라는 가정을 명시)을 넣기 위해 if-then 문장으로 재구성
 	applyDiscount(aNumber) {
-		return this.discountRate ? aNumber - this.discountRate * aNumber : aNumber;
+		if (!this.discountRate) return aNumber;
+		else return aNumber - this.discountRate * aNumber;
 	}
 }
 
