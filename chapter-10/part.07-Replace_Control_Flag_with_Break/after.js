711968975845202e7eaa4f2c7225a74d62a636c1
imseongtae <dev.haemil@gmail.com>
2020-10-25 00:18:16 +0900

    refactor: Refactor test code

--- chapter-10/part.07-Replace_Control_Flag_with_Break/after.js
+++ chapter-10/part.07-Replace_Control_Flag_with_Break/after.js
@@ -1,22 +1,17 @@
 // 함수 추출하기 기법을 통해 밀접한 코드만 함수로 추출
 function checkForMiscreants(people) {
-	// 제어 플래그를 제거했다면, 이를 참조하는 다른 코드도 모두 제거
 	for (const p of people) {
 		if (p === 'joker') {
-			// found = true;
-			sendAlert(p);
-			return;
+			return sendAlert();
 		}
 		if (p === 'ham') {
-			// 제어 변수가 갱신되는 곳은 같은 과정을 진행
-			sendAlert(p);
-			return;
+			return sendAlert();
 		}
 	}
 }
 
-function sendAlert(villain) {
-	return villain;
+function sendAlert() {
+	return true;
 }
 
 module.exports = checkForMiscreants;
