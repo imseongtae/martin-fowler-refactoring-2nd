4de989b685536b0a25c97539465c0782320e6e61
imseongtae <dev.haemil@gmail.com>
2020-10-25 00:06:29 +0900

    refactor: 제어 변수가 참이면 반복문에서는 할 일이 없으므로 함수에서 아예 빠져나온다

--- chapter-10/part.07-Replace_Control_Flag_with_Break/after.js
+++ chapter-10/part.07-Replace_Control_Flag_with_Break/after.js
@@ -1,11 +1,14 @@
+// 함수 추출하기 기법을 통해 밀접한 코드만 함수로 추출
 function checkForMiscreants(people) {
 	// 생략(중요하지 않은 코드)
 	let found = false;
 	for (const p of people) {
 		if (!found) {
 			if (p === 'joker') {
-				found = true;
-				return sendAlert(p);
+				// found = true;
+				// 제어 변수가 참이면 반복문에서는 할 일이 없으므로 함수에서 아예 빠져나온다
+				sendAlert(p);
+				return;
 			}
 			if (p === 'ham') {
 				found = true;
