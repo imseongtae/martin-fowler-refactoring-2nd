fb849064d3f44192bc774de7ecf7f7fe21b0bf32
imseongtae <dev.haemil@gmail.com>
2020-10-25 00:12:01 +0900

    refactor: 제어 플래그를 제거했다면, 이를 참조하는 다른 코드도 모두 제거

--- chapter-10/part.07-Replace_Control_Flag_with_Break/after.js
+++ chapter-10/part.07-Replace_Control_Flag_with_Break/after.js
@@ -1,22 +1,18 @@
 // 함수 추출하기 기법을 통해 밀접한 코드만 함수로 추출
 function checkForMiscreants(people) {
-	// 생략(중요하지 않은 코드)
-	let found = false;
+	// 제어 플래그를 제거했다면, 이를 참조하는 다른 코드도 모두 제거
 	for (const p of people) {
-		if (!found) {
-			if (p === 'joker') {
-				// found = true;
-				sendAlert(p);
-				return;
-			}
-			if (p === 'ham') {
-				// 제어 변수가 갱신되는 곳은 같은 과정을 진행
-				sendAlert(p);
-				return;
-			}
+		if (p === 'joker') {
+			// found = true;
+			sendAlert(p);
+			return;
+		}
+		if (p === 'ham') {
+			// 제어 변수가 갱신되는 곳은 같은 과정을 진행
+			sendAlert(p);
+			return;
 		}
 	}
-	return found;
 }
 
 function sendAlert(villain) {
