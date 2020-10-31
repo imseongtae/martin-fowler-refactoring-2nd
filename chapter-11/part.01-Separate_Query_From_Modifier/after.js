afc600ddc25becf81768ee1d8fc78598581f2cf2
imseongtae <dev.haemil@gmail.com>
2020-11-01 01:11:51 +0900

    refactor: 원래 함수를 호출하는 곳에서 새로운 질의함수를 호출하도록 바꾸고, 원래 함수에서 질의 관련 코드를 없앤다

--- chapter-11/part.01-Separate_Query_From_Modifier/after.js
+++ chapter-11/part.01-Separate_Query_From_Modifier/after.js
@@ -1,19 +1,21 @@
+// 원래 함수를 호출하는 곳에서 새로운 질의함수를 호출하도록 바꾸고, 원래 함수에서 질의 관련 코드를 없앤다.
 function alertForMiscreant(people) {
 	for (const p of people) {
 		if (p === 'joker') {
-			setOffAlarms();
-			return 'joker';
+			return setOffAlarms();
+			// return;
 		}
 		if (p === 'ham') {
-			setOffAlarms();
-			return 'ham';
+			return setOffAlarms();
+			// return;
 		}
 	}
-	return '';
+	return;
 }
 
 function setOffAlarms() {
-	console.log('find miscreant');
+	// console.log('find miscreant');
+	return 'find miscreant';
 }
 
 // 1. 함수를 복제하고 질의 목적에 맞는 이름 짓기
@@ -32,4 +34,4 @@ function findMiscreant(people) {
 	return '';
 }
 
-module.exports = findMiscreant;
+module.exports = { findMiscreant, alertForMiscreant };
