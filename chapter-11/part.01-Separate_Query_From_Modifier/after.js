86302011d3c48aefa5025a5b21447e37ab476cbe
imseongtae <dev.haemil@gmail.com>
2020-11-01 00:59:57 +0900

    refactor: 새 질의 함수에서 부수효과를 낳는 부분 제거

--- chapter-11/part.01-Separate_Query_From_Modifier/after.js
+++ chapter-11/part.01-Separate_Query_From_Modifier/after.js
@@ -17,14 +17,15 @@ function setOffAlarms() {
 }
 
 // 1. 함수를 복제하고 질의 목적에 맞는 이름 짓기
+// 2. 새 질의 함수에서 부수효과를 낳는 부분 제거
 function findMiscreant(people) {
 	for (const p of people) {
 		if (p === 'joker') {
-			setOffAlarms();
+			// setOffAlarms();
 			return 'joker';
 		}
 		if (p === 'ham') {
-			setOffAlarms();
+			// setOffAlarms();
 			return 'ham';
 		}
 	}
