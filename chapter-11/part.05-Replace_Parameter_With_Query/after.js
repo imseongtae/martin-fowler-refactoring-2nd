a32243e838bb2bad7d5e90fccd7b75fc0070e70f
imseongtae <dev.haemil@gmail.com>
2020-11-04 21:13:47 +0900

    refactor: discountLevel 을 필요할 때 직접 호출하도록 수정, 매개변수 대신 함수 호출로 수정

--- chapter-11/part.05-Replace_Parameter_With_Query/after.js
+++ chapter-11/part.05-Replace_Parameter_With_Query/after.js
@@ -7,14 +7,15 @@ class Order {
 	// 함수 간소화를 위해 임시 변수를 질의 함수로 바꾸기 적용
 	get finalPrice() {
 		const basePrice = this.quantity * this.itemPrice;
-		return this.discountedPrice(basePrice, this.discountLevel);
+		return this.discountedPrice(basePrice); // 필요할 때 호출하도록 수정
 	}
 	get discountLevel() {
 		return this.quantity > 100 ? 2 : 1;
 	}
 
-	discountedPrice(basePrice, discountLevel) {
-		switch (discountLevel) {
+	// discountLevel 을 필요할 때 직접 호출하도록 수정, 매개변수 대신 함수 호출로 수정
+	discountedPrice(basePrice) {
+		switch (this.discountLevel) {
 			case 1:
 				return basePrice * 0.95;
 			case 2:
