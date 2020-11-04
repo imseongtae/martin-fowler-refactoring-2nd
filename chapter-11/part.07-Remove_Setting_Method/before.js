74bbc1a6f77f8c3649fe9fdddde674c355fde293
imseongtae <dev.haemil@gmail.com>
2020-11-04 20:54:29 +0900

    feat: Add original code

---
+++ chapter-11/part.07-Remove_Setting_Method/before.js
@@ -0,0 +1,16 @@
+class Person {
+	get name() {
+		return this._name;
+	}
+	set name(arg) {
+		this._name = arg;
+	}
+	get id() {
+		return this._id;
+	}
+	set id(arg) {
+		this._id = arg;
+	}
+}
+
+module.exports = Person;
