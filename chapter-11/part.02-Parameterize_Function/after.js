e54c28ca45f5d7e83eaaec5e661210bf7f8fd4e2
imseongtae <dev.haemil@gmail.com>
2020-11-04 18:36:03 +0900

    refactor: 대역의 하한을 호출하는 부분도 새로 만든 매개변수화 함수를 호출하도록 수정

--- chapter-11/part.02-Parameterize_Function/after.js
+++ chapter-11/part.02-Parameterize_Function/after.js
@@ -4,16 +4,20 @@ function withinBand(usage, bottom, top) {
 }
 
 // 함수 선언 바꾸기를 적용하고, 리터럴들을 호출 시점에 바꾸도록 적용
+// 대역의 하한을 호출하는 부분도 새로 만든 매개변수화 함수를 호출하도록 수정
 function baseCharge(usage) {
 	if (usage < 0) return usd(0);
 	const amount =
-		bottomBand(usage) * 0.03 + withinBand(usage, 100, 200) * 0.05 + topBand(usage) * 0.07;
+		withinBand(usage, 0, 100) * 0.03 +
+		withinBand(usage, 100, 200) * 0.05 +
+		topBand(usage) * 0.07;
 	return usd(amount);
 }
 
-function bottomBand(usage) {
-	return Math.min(usage, 100);
-}
+// 기존 대역 하한을 호출하는 함수 주석처리
+// function bottomBand(usage) {
+// 	return Math.min(usage, 100);
+// }
 
 function middleBand(usage) {
 	return usage > 100 ? Math.min(usage, 200) - 100 : 0;
