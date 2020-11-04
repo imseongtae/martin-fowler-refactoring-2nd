4fd581d5f5663bdab085242c677c026a2d6e33f5
imseongtae <dev.haemil@gmail.com>
2020-11-04 18:42:16 +0900

    refactor: 대역의 상한을 호출할 때는 Infinity 사용

--- chapter-11/part.02-Parameterize_Function/after.js
+++ chapter-11/part.02-Parameterize_Function/after.js
@@ -4,28 +4,25 @@ function withinBand(usage, bottom, top) {
 }
 
 // 함수 선언 바꾸기를 적용하고, 리터럴들을 호출 시점에 바꾸도록 적용
-// 대역의 하한을 호출하는 부분도 새로 만든 매개변수화 함수를 호출하도록 수정
 function baseCharge(usage) {
 	if (usage < 0) return usd(0);
 	const amount =
 		withinBand(usage, 0, 100) * 0.03 +
 		withinBand(usage, 100, 200) * 0.05 +
-		topBand(usage) * 0.07;
+		withinBand(usage, 200, Infinity) * 0.07; // 대역의 상한을 호출할 때는 Infinity 사용
 	return usd(amount);
 }
 
-// 기존 대역 하한을 호출하는 함수 주석처리
+// 초기의 구문이 논리적으로 필요없을지라도 예외 상황에서의 대처 방식을 설명해주므로 그냥 두기로 결정
 // function bottomBand(usage) {
 // 	return Math.min(usage, 100);
 // }
-
-function middleBand(usage) {
-	return usage > 100 ? Math.min(usage, 200) - 100 : 0;
-}
-
-function topBand(usage) {
-	return usage > 200 ? usage - 200 : 0;
-}
+// function middleBand(usage) {
+// 	return usage > 100 ? Math.min(usage, 200) - 100 : 0;
+// }
+// function topBand(usage) {
+// 	return usage > 200 ? usage - 200 : 0;
+// }
 
 function usd(amount) {
 	return amount + ' usd';
