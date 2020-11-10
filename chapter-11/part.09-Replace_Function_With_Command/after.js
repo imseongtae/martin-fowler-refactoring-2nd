b142dd71652b2ce592ff526056b10f0aaad7839f
imseongtae <dev.haemil@gmail.com>
2020-11-10 23:38:43 +0900

    refactor: 다른 매개변수들도 옮기기

--- chapter-11/part.09-Replace_Function_With_Command/after.js
+++ chapter-11/part.09-Replace_Function_With_Command/after.js
@@ -2,26 +2,29 @@
 
 function score(candidate, medicalExam, scoringGuide) {
 	// execute() 메서드가 매개변수를 받지 않도록 수정, 매개변수가 복잡할 때는 편리해짐
-	return new Scorer(candidate).execute(medicalExam, scoringGuide);
+	// 다른 매개변수들도 옮기기
+	return new Scorer(candidate, medicalExam, scoringGuide).execute();
 }
 
 // 1. 빈 클래스를 만들고, 2. score함수를 클래스로 옮긴다.
 class Scorer {
-	constructor(candidate) {
+	constructor(candidate, medicalExam, scoringGuide) {
 		this._candidate = candidate;
+		this._medicalExam = medicalExam;
+		this._scoringGuide = scoringGuide;
 	}
-	execute(medicalExam, scoringGuide) {
+	execute(scoringGuide) {
 		let result = 0;
 		let healthLevel = 0;
 		let highMedicalRiskFlag = false;
 
-		if (medicalExam.isSmoker) {
+		if (this._medicalExam.isSmoker) {
 			healthLevel += 10;
 			highMedicalRiskFlag = true;
 		}
 
 		let certificationGrade = 'regular';
-		if (scoringGuide.stateWithLowCertification(this._candidate.originState)) {
+		if (this._scoringGuide.stateWithLowCertification(this._candidate.originState)) {
 			certificationGrade = 'low';
 			result -= 5;
 		}
