a278041440b1516d36bea95112f89a9eeb8ab80b
imseongtae <dev.haemil@gmail.com>
2020-11-11 00:14:22 +0900

    refactor: 더 가다듬기, 모든 지역변수를 필드로 바꾸기

--- chapter-11/part.09-Replace_Function_With_Command/after.js
+++ chapter-11/part.09-Replace_Function_With_Command/after.js
@@ -7,6 +7,7 @@ function score(candidate, medicalExam, scoringGuide) {
 }
 
 // 1. 빈 클래스를 만들고, 2. score함수를 클래스로 옮긴다.
+// 더 가다듬기, 모든 지역변수를 필드로 바꾸기
 class Scorer {
 	constructor(candidate, medicalExam, scoringGuide) {
 		this._candidate = candidate;
@@ -14,23 +15,23 @@ class Scorer {
 		this._scoringGuide = scoringGuide;
 	}
 	execute(scoringGuide) {
-		let result = 0;
-		let healthLevel = 0;
-		let highMedicalRiskFlag = false;
+		this._result = 0; // let result = 0;
+		this._healthLevel = 0;
+		this._highMedicalRiskFlag = false;
 
 		if (this._medicalExam.isSmoker) {
-			healthLevel += 10;
-			highMedicalRiskFlag = true;
+			this._healthLevel += 10;
+			this._highMedicalRiskFlag = true;
 		}
 
-		let certificationGrade = 'regular';
+		this._certificationGrade = 'regular';
 		if (this._scoringGuide.stateWithLowCertification(this._candidate.originState)) {
-			certificationGrade = 'low';
-			result -= 5;
+			this._certificationGrade = 'low';
+			this._result -= 5;
 		}
 		// 비슷한 코드가 한참 이어짐
-		result -= Math.max(healthLevel - 5, 0);
-		return result;
+		this._result -= Math.max(this._healthLevel - 5, 0);
+		return this._result;
 	}
 }
 
