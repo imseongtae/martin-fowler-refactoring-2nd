19d6739929823f2b40742f66a375556ad582b3aa
imseongtae <dev.haemil@gmail.com>
2020-11-10 22:40:51 +0900

    refactor: 빈 클래스를 만들고, score함수를 클래스로 옮긴다

--- chapter-11/part.09-Replace_Function_With_Command/after.js
+++ chapter-11/part.09-Replace_Function_With_Command/after.js
@@ -1,23 +1,30 @@
 /* eslint-disable no-unused-vars */
 
 function score(candidate, medicalExam, scoringGuide) {
-	let result = 0;
-	let healthLevel = 0;
-	let highMedicalRiskFlag = false;
+	return new Scorer().execute(candidate, medicalExam, scoringGuide);
+}
 
-	if (medicalExam.isSmoker) {
-		healthLevel += 10;
-		highMedicalRiskFlag = true;
-	}
+// 1. 빈 클래스를 만들고, 2. score함수를 클래스로 옮긴다.
+class Scorer {
+	execute(candidate, medicalExam, scoringGuide) {
+		let result = 0;
+		let healthLevel = 0;
+		let highMedicalRiskFlag = false;
 
-	let certificationGrade = 'regular';
-	if (scoringGuide.stateWithLowCertification(candidate.originState)) {
-		certificationGrade = 'low';
-		result -= 5;
+		if (medicalExam.isSmoker) {
+			healthLevel += 10;
+			highMedicalRiskFlag = true;
+		}
+
+		let certificationGrade = 'regular';
+		if (scoringGuide.stateWithLowCertification(candidate.originState)) {
+			certificationGrade = 'low';
+			result -= 5;
+		}
+		// 비슷한 코드가 한참 이어짐
+		result -= Math.max(healthLevel - 5, 0);
+		return result;
 	}
-	// 비슷한 코드가 한참 이어짐
-	result -= Math.max(healthLevel - 5, 0);
-	return result;
 }
 
 module.exports = score;
