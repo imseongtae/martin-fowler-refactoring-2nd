31b96a206fd77f51b6c84a6ba400df727d35f7f9
imseongtae <dev.haemil@gmail.com>
2020-11-11 17:17:36 +0900

    refactor: 최상위 charge() 함수로 인라인

--- chapter-11/part.10-Replace_Command_With_Function/after.js
+++ chapter-11/part.10-Replace_Command_With_Function/after.js
@@ -1,11 +1,11 @@
 class ChargeCalculator {
 	// 함수 선언 바꾸기를 통해 생성자가 받던 모든 변수를 charge() 메서드로 옮기기
-	constructor(customer, usage, provider) {
-		// 필드는 가급적 지우기
-		// this._customer = customer;
-		// this._usage = usage;
-		// this._provider = provider;
-	}
+	// constructor(customer, usage, provider) {
+	// 	// 필드는 가급적 지우기
+	// 	// this._customer = customer;
+	// 	// this._usage = usage;
+	// 	// this._provider = provider;
+	// }
 
 	// baseCharge() 같이 값을 반환하는 보조 메서드는 변수로 추출!
 	charge(customer, usage, provider) {
@@ -16,11 +16,9 @@ class ChargeCalculator {
 }
 
 function charge(customer, usage, provider) {
-	return new ChargeCalculator(customer, usage, provider).charge(
-		customer,
-		usage,
-		provider,
-	);
+	// 최상위 charge() 함수로 인라인
+	const baseCharge = customer.baseRate * usage;
+	return baseCharge + provider.connectionCharge;
 }
 
 module.exports = charge;
