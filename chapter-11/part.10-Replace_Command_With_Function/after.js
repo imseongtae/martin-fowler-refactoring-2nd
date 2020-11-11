2c56af6d214158780961a79ef975550b9c39d409
imseongtae <dev.haemil@gmail.com>
2020-11-11 17:11:20 +0900

    refactor: charge() 본문에서 필드 대신 건네받은 매개변수를 사용하도록 수정

--- chapter-11/part.10-Replace_Command_With_Function/after.js
+++ chapter-11/part.10-Replace_Command_With_Function/after.js
@@ -1,21 +1,26 @@
 class ChargeCalculator {
 	// 함수 선언 바꾸기를 통해 생성자가 받던 모든 변수를 charge() 메서드로 옮기기
 	constructor(customer, usage, provider) {
-		this._customer = customer;
-		this._usage = usage;
-		this._provider = provider;
+		// 필드는 가급적 지우기
+		// this._customer = customer;
+		// this._usage = usage;
+		// this._provider = provider;
 	}
 
 	// baseCharge() 같이 값을 반환하는 보조 메서드는 변수로 추출!
-	get charge(customer, usage, provider) {
-		// baseCharge() 추출 이후 해당 보조 메서드를 인라인
-		const baseCharge = this._customer.baseRate * this._usage;
-		return baseCharge + this._provider.connectionCharge;
+	charge(customer, usage, provider) {
+		// charge() 본문에서 필드 대신 건네받은 매개변수를 사용하도록 수정
+		const baseCharge = customer.baseRate * usage;
+		return baseCharge + provider.connectionCharge;
 	}
 }
 
 function charge(customer, usage, provider) {
-	return new ChargeCalculator(customer, usage, provider).charge(customer, usage, provider);
+	return new ChargeCalculator(customer, usage, provider).charge(
+		customer,
+		usage,
+		provider,
+	);
 }
 
 module.exports = charge;
