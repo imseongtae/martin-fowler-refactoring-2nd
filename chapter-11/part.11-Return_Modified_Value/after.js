fb156b40a25fb96e7d901e37b2a38b8da99ae756
imseongtae <dev.haemil@gmail.com>
2020-11-11 18:28:29 +0900

    refactor: 반환할 값을 담을 변수를 생성하고 이름짓기 규칙에 맞도록 내용을 수정

--- chapter-11/part.11-Return_Modified_Value/after.js
+++ chapter-11/part.11-Return_Modified_Value/after.js
@@ -17,11 +17,14 @@ const pace = totalTime / 60 / totalDistance;
 // 이번 리팩터링에서는 고도 상승분 계산만을 고려함
 // calculateAscent()와 외부 환경이 어떻게 연결돼 있는지 숨겨져 있으므로 갱신 사실을 드러내기
 function calculateAscent() {
+	// totalAscent = 0; // 반환할 값을 담을 변수를 생성! 하지만 더 나은 변수명을 위해 수정
+	// 일반적인 이름짓기 규칙에 맞도록 내용을 수정
+	let result = 0;
 	for (let i = 1; i < points.length; i++) {
 		const verticalChange = points[i].elevation - points[i - 1].elevation;
-		totalAscent += verticalChange > 0 ? verticalChange : 0;
+		result += verticalChange > 0 ? verticalChange : 0;
 	}
-	return totalAscent; // totalAscent 값을 반환
+	return result; // totalAscent 값을 반환
 }
 
 function calculateTime() {
