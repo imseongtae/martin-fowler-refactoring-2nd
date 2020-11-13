f7b2e7a626eecd0e299132f51090736f765804ea
imseongtae <dev.haemil@gmail.com>
2020-11-13 11:35:11 +0900

    refactor: annualCost() 메서드를 슈퍼 클래스에 복사하고, 서브 클래스에서 제거 후 테스트

--- chapter-12/part.01-Pull_Up_Method/after.js
+++ chapter-12/part.01-Pull_Up_Method/after.js
@@ -2,13 +2,17 @@ class Party {
 	constructor() {
 		this.monthlyCost = 1;
 	}
+	// annualCost() 메서드를 슈퍼 클래스에 복사하고, 서브 클래스에서 제거 후 테스트
+	get annualCost() {
+		return this.monthlyCost * 12;
+	}
 }
 
 class Employee extends Party {
 	// 두 메서드의 이름을 통일한다
-	get annualCost() {
-		return this.monthlyCost * 12;
-	}
+	// get annualCost() {
+	// 	return this.monthlyCost * 12;
+	// }
 }
 
 class Department extends Party {
